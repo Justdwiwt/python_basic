d112d9241808e98c91ffd6669cbe1b6342e1104a
Justdwiwt <len_master@hotmail.com>
2018-09-18 19:33:29 +0800

    refactor MovieTop.py

--- bugs/MovieTop.py
+++ bugs/MovieTop.py
@@ -4,7 +4,6 @@
 import re
 from urllib import request
 
-# noinspection PyUnresolvedReferences
 from common.mysql_client import *
 
 
@@ -15,20 +14,19 @@ class MovieTop(object):
         self.start = 0
         self.param = '&filter='
         self.headers = {'User-Agent': 'Mozilla/5.0(Windows NT 6.1;WOW64)'}
+        self.movie_list = []
+        self.file_path = 'D:\op205.txt'
 
     def get_page(self):
-        page_content = []
         try:
-            while self.start <= 225:
-                url = 'https://movie.douban.com/top250?start=' + str(self.start)
-                req = request.Request(url, headers=self.headers)
-                response = request.urlopen(req)
-                page = response.read().decode('utf-8')
-                page_num = (self.start + 25) // 25
-                print('正在抓取第' + str(page_num) + '页数据...')
-                self.start += 25
-                page_content.append(page)
-                return page_content
+            url = 'https://movie.douban.com/top250?start=' + str(self.start)
+            req = request.Request(url, headers=self.headers)
+            response = request.urlopen(req)
+            page = response.read().decode('utf-8')
+            page_num = (self.start + 25) // 25
+            print('正在抓取第' + str(page_num) + '页数据...')
+            self.start += 25
+            return page
         except request.URLError as e:
             if hasattr(e, 'reason'):
                 print('抓取失败，失败原因：', e.reason)
@@ -68,31 +66,48 @@ class MovieTop(object):
                                     movie[9],
                                     movie[10]])
 
-    def write_text(self):
-        print('开始向文件写入数据......')
-        file_top = open(self.file_path, 'w', encoding='utf-8')
+    # def write_text(self):
+    #     print('开始向文件写入数据......')
+    #     file_top = open(self.file_path, 'w', encoding='utf-8')
+    #     try:
+    #         for movie in self.movie_list:
+    #             file_top.write('电影排名：' + movie[0] + '\r\n')
+    #             file_top.write('电影名称：' + movie[1] + '\r\n')
+    #             file_top.write('外文名称：' + movie[2] + '\r\n')
+    #             file_top.write('电影别名：' + movie[3] + '\r\n')
+    #             file_top.write('导演姓名：' + movie[4] + '\r\n')
+    #             file_top.write('上映年份：' + movie[5] + '\r\n')
+    #             file_top.write('制作国家/地区：' + movie[6] + '\r\n')
+    #             file_top.write('电影类别：' + movie[7] + '\r\n')
+    #             file_top.write('电影评分：' + movie[8] + '\r\n')
+    #             file_top.write('参评人数：' + movie[9] + '\r\n')
+    #             file_top.write('简短影评：' + movie[10] + '\r    \n')
+    #         print('抓取结果写入文件成功...')
+    #     except Exception as e:
+    #         print(e)
+    #     finally:
+    #         file_top.close()
+
+    # noinspection PyUnboundLocalVariable,SpellCheckingInspection
+    def insert_into_mysql(self):
+        db = pymysql.connect('localhost', 'root', '1234', 'db_py_basic')
+        cursor = db.cursor()
+        insert_str = "insert into douban_comment(movieRank, movieName, movieForName, movieAlias," \
+                     " directorName, showYear, makeCounty, movieType, movieScore, scoreNum, shortFilm) " \
+                     "values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
         try:
             for movie in self.movie_list:
-                file_top.write('电影排名：' + movie[0] + '\r\n')
-                file_top.write('电影名称：' + movie[1] + '\r\n')
-                file_top.write('外文名称：' + movie[2] + '\r\n')
-                file_top.write('电影别名：' + movie[3] + '\r\n')
-                file_top.write('导演姓名：' + movie[4] + '\r\n')
-                file_top.write('上映年份：' + movie[5] + '\r\n')
-                file_top.write('制作国家/地区：' + movie[6] + '\r\n')
-                file_top.write('电影类别：' + movie[7] + '\r\n')
-                file_top.write('电影评分：' + movie[8] + '\r\n')
-                file_top.write('参评人数：' + movie[9] + '\r\n')
-                file_top.write('简短影评：' + movie[10] + '\r    \n')
-            print('抓取结果写入文件成功...')
+                insert_sql = insert_str % (
+                    movie[0], movie[1], movie[2], movie[3], movie[4], movie[5], movie[6], movie[7], movie[8], movie[9],
+                    movie[10])
+            cursor.execute(insert_sql)
         except Exception as e:
             print(e)
-        finally:
-            file_top.close()
 
     def main(self):
         print('开始从豆瓣电影抓取数据......')
-        self.write_text()
+        self.get_movie_info()
+        self.insert_into_mysql()
         print('数据抓取完毕...')
         dou_ban_spider = MovieTop()
         dou_ban_spider.main()
