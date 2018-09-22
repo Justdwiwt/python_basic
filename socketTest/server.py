fdd9317a743f121bc648f1b87a8c73d25a2511d2
Justdwiwt <len_master@hotmail.com>
2018-09-22 19:34:31 +0800

    模拟socket连接

---
+++ socketTest/server.py
@@ -0,0 +1,20 @@
+# ！ /usr/bin/python3
+# -*-coding:UTF-8-*-
+
+import socket
+import datetime
+
+HOST = '0.0.0.0'
+PORT = 3434
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+while True:
+    conn, addr = s.accept()
+    print('Client %s connected!' % str(addr))
+    dt = datetime.datetime.now()
+    message = 'Current time is' + str(dt)
+    # noinspection PyTypeChecker
+    conn.send(message)
+    print('Sent: ', message)
+    conn.close()
